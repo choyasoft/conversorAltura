48dcf403f04857962667ae9b0d06bc68594fd0ed
ivy <ivy@ChoyaPC>
2020-09-23 19:31:48 +0200

    Added a try/catch exception. Minor font changes

--- src/Height/Converter/heightconverter.java
+++ src/Height/Converter/heightconverter.java
@@ -37,6 +37,7 @@ public class heightconverter extends javax.swing.JFrame {
         SalirBtn = new javax.swing.JButton();
         pulgText = new javax.swing.JTextField();
         cmLabel = new javax.swing.JLabel();
+        commentLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Conversor de pulgadas y pies");
@@ -48,6 +49,12 @@ public class heightconverter extends javax.swing.JFrame {
 
         piesLabel.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
         piesLabel.setText("Pies");
+
+        piesText.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                piesTextActionPerformed(evt);
+            }
+        });
 
         pulgLabel.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
         pulgLabel.setText("Pulgadas");
@@ -77,36 +84,43 @@ public class heightconverter extends javax.swing.JFrame {
             }
         });
 
+        cmLabel.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
         cmLabel.setText("Centimetros");
+
+        commentLabel.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(pulgLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE)
-                    .addComponent(piesLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(piesText, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
-                    .addComponent(pulgText))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(16, 16, 16)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(cmLabel)
-                        .addGap(18, 18, 18)
-                        .addComponent(cmText, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(64, 64, 64))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(vaciarBtn)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(SalirBtn))
-                        .addComponent(convertBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 296, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(16, 16, 16)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(cmLabel)
+                                .addGap(18, 18, 18)
+                                .addComponent(cmText, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(64, 64, 64))
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(jPanel1Layout.createSequentialGroup()
+                                    .addComponent(vaciarBtn)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(SalirBtn))
+                                .addComponent(convertBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 296, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(pulgLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE)
+                                    .addComponent(piesLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(piesText, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
+                                    .addComponent(pulgText)))
+                            .addComponent(commentLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(25, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -127,10 +141,12 @@ public class heightconverter extends javax.swing.JFrame {
                     .addComponent(cmText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(cmLabel))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(commentLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(vaciarBtn)
                     .addComponent(SalirBtn))
-                .addContainerGap(46, Short.MAX_VALUE))
+                .addGap(19, 19, 19))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -149,11 +165,19 @@ public class heightconverter extends javax.swing.JFrame {
 
     private void convertBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_convertBtnActionPerformed
        double pies,pulgadas,centimetros;
+       
+       try{
         pies = Double.parseDouble(piesText.getText());
         pulgadas = Double.parseDouble(pulgText.getText());
         centimetros = (pies * 12 +pulgadas) * 2.54;
         cmText.setText(String.valueOf(centimetros));
-        
+        commentLabel.setText("");
+       }catch (Exception e){
+               commentLabel.setText("Revisa los parámetros requeridos.");
+               piesText.setText("");
+               pulgText.setText("");
+               cmText.setText("");
+               }
         
     }//GEN-LAST:event_convertBtnActionPerformed
 
@@ -170,6 +194,10 @@ public class heightconverter extends javax.swing.JFrame {
         System.exit(0);
     
     }//GEN-LAST:event_SalirBtnActionPerformed
+
+    private void piesTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_piesTextActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_piesTextActionPerformed
 
     /**
      * @param args the command line arguments
@@ -210,6 +238,7 @@ public class heightconverter extends javax.swing.JFrame {
     private javax.swing.JButton SalirBtn;
     private javax.swing.JLabel cmLabel;
     private javax.swing.JTextField cmText;
+    private javax.swing.JLabel commentLabel;
     private javax.swing.JButton convertBtn;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel piesLabel;
